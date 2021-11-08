4fb65f4d71fc41280bdd554ff42245cec7b9539d
Gustavo Luis <gustavodev2020@gmail.com>
2021-11-08 16:26:57 -0300

    nameapp

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Scan Words',
+        title: 'Scan Word',
         theme: ThemeData(
           primarySwatch: Colors.orange,
         ),
